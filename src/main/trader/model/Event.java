ae4101115abae3c716b260aaff1b5af5c73cbd87
davidcaiqifan <caid100@gmail.com>
2021-05-13 21:54:36 +0800

    Added wrapper class for trade and orderbook events

--- src/main/trader/model/Event.java
+++ src/main/trader/model/Event.java
@@ -1,13 +1,23 @@
 package model;
 
+import com.binance.api.client.domain.event.AggTradeEvent;
 import com.binance.api.client.domain.event.DepthEvent;
 
 public class Event {
     private DepthEvent depthEvent;
-    public Event(DepthEvent event) {
+    private AggTradeEvent tradeEvent;
+    public Event(DepthEvent depthEvent) {
+        this.depthEvent = depthEvent;
+    }
+
+    public Event(AggTradeEvent tradeEvent) {
+        this.tradeEvent = tradeEvent;
     }
     public DepthEvent getDepthEvent() {
         return depthEvent;
     }
 
+    public AggTradeEvent getTradeEvent() {
+        return tradeEvent;
+    }
 }
